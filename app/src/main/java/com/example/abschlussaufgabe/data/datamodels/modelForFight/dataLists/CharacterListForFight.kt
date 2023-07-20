346b0511e6fcdf8e71c175a30ad768044c5b201a
Angelique Freier <freier-uede@web.de>
2023-07-20 17:59:30 +0200

    Funktionen für Bildwechsel nach bestimmter Zeit eingebaut

--- app/src/main/java/com/example/abschlussaufgabe/data/datamodels/modelForFight/dataLists/CharacterListForFight.kt
+++ app/src/main/java/com/example/abschlussaufgabe/data/datamodels/modelForFight/dataLists/CharacterListForFight.kt
@@ -12,7 +12,7 @@ class CharacterListForFight {
         mapOf("Rasengan" to 50, "Rasenshuriken" to 70, "Jutsu des vertrauten Geistes" to 80, "Taijutsu" to 20),
         mapOf("Shuriken" to 20, "Kunai" to 15),
         mapOf("Weisenmodus" to 80, "Kiubimodus" to 100),
-        listOf("Schattendoppelgänger", "Jutsu des Tausches"),
+        listOf("Jutsu des Tausches", "Schattendoppelgänger"),
         R.raw.song_naruto, R.drawable.naruto_pose,
             500, 500
     ),
@@ -23,7 +23,7 @@ class CharacterListForFight {
         mapOf("Chidori" to 50, "Feuerversteck" to 40, "Jutsu des vertrauten Geistes" to 80, "Taijutsu" to 30),
         mapOf("Shuriken" to 20, "Schwert" to 30),
         mapOf("Susanoo" to 100),
-        listOf("Schattendoppelgänger", "Jutsu des Tausches"),
+        listOf("Jutsu des Tausches", "Schattendoppelgänger"),
         R.raw.song_sasuke, R.drawable.sasuke_pose,
         500, 500
     ),
@@ -34,7 +34,7 @@ class CharacterListForFight {
         mapOf("große Sakura" to 20, "Kirschblütenschlag" to 50, "Kirschblütenformation" to 70, "Jutsu des vertrauten Geistes" to 80, "Taijutsu" to 15),
         mapOf("Shuriken" to 20, "Kunai" to 15),
         mapOf("Heilung" to 80),
-        listOf("Schattendoppelgänger", "Jutsu des Tausches"),
+        listOf("Jutsu des Tausches", "Schattendoppelgänger"),
         R.raw.song_sakura, R.raw.video_sakura,
         500, 500
     )
